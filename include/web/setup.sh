6a7c2c4fa6d516a923d64310d56fa3f9fe2f7dd9
duhow <contacto@duhowpi.net>
2021-06-25 20:24:50 +0200

    Tweak web setup script

--- include/web/setup.sh
+++ include/web/setup.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
-mkdir -p /var/www/cgi-bin
 
-cp index.html /var/www
-cp run.cgi /var/www/cgi-bin
+WEBFOLDER=/var/www
+SRCFOLDER=/usr/web
 
-busybox httpd -p 81 -h /var/www
+mkdir -p ${WEBFOLDER}/cgi-bin
+
+cp -fv ${SRCFOLDER}/index.html ${WEBFOLDER}
+cp -fv ${SRCFOLDER}/run.cgi ${WEBFOLDER}/cgi-bin
+
+PORT=80
+
+# if port 80 is in use, fallback to 81
+netstat -ltn | grep -q ":${PORT} " && PORT=81
+
+busybox httpd -p ${PORT} -h ${WEBFOLDER}

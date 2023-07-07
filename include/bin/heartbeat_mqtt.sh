0d79a1d6ead1c99c629f41c8e90d2b2c29d4c825
duhow <contacto@duhowpi.net>
2023-07-07 16:01:45 +0200

    Update MQTT availability in heartbeat

--- include/bin/heartbeat_mqtt.sh
+++ include/bin/heartbeat_mqtt.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 LOCKFILE=/tmp/heartbeat.lock
+PIDFILE=/tmp/listener_mqtt.pid
 log(){ echo "$*" | tee /dev/kmsg; }
 
 WAIT=60
@@ -15,8 +16,20 @@ TOPIC=`mqtt_base_topic`
 STATUS=$(mosquitto_sub ${MQTT_OPTS} -I heartbeat -v -C 1 -t "${TOPIC}" -W ${TIMEOUT} | cut -d ' ' -f2)
 
 if [ ! -f "$LOCKFILE" ] && [ "$STATUS" = "offline" ]; then
+  STATUS=""
   log "Disconnected from MQTT. Rebooting in ${WAIT} seconds."
   wifi_led red
   sleep ${WAIT}
   reboot
+elif [ -z "$STATUS" ] && [ -e "/proc/`cat ${PIDFILE}`" ]; then
+  STATUS=online
+elif [ "$STATUS" == "online" ] && [ ! -e "/proc/`cat ${PIDFILE}`" ]; then
+  STATUS=offline
+else
+  STATUS=""
+fi
+
+if [ -n "$STATUS" ]; then
+  log "Setting status as ${STATUS}"
+  mosquitto_pub ${MQTT_OPTS} -t "`mqtt_base_topic`" -m ${STATUS}
 fi
